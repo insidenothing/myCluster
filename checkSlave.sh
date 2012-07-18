c11e264d4f0cbe3214c784469e1a9b8fffa83d2b
Patrick McGuire <insidenothing@gmail.com>
2012-07-18 11:05:41 -0300

    use variables

--- checkSlave.sh
+++ checkSlave.sh
@@ -3,7 +3,7 @@
 source myCluster.conf
 
 echo "check status of db1"
-mysql --user=[mysqlusername] --password=[mysqlpassword] < /root/checkSlave.sql > db1Status.txt
+mysql --user=$username --password=$password < /root/checkSlave.sql > db1Status.txt
 
 echo "Parsing master information from db1"
 php -f /root/checkSlave.php
